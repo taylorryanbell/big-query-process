926ae2da53d1a3cf6e4ec3ccef819626c19f6e60
Taylor Bell <taylor.ryan.bell@gmail.com>
2022-01-05 10:35:44 -0600

    setting up

---
+++ main.py
@@ -0,0 +1,19 @@
+#
+# BigQuery Process
+# Taylor Bell
+# 2022.1.5
+#
+
+# imports
+import apache_beam as beam
+
+
+def run():
+    with beam.Pipeline() as pipeline:
+
+    pass
+
+
+if __name__ == '__main__':
+    run()
+    pass
